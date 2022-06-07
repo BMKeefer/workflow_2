2d16adfee3a24c93adcf26a03286dd43d289c243
BMKeefer <106254465+BMKeefer@users.noreply.github.com>
2022-06-07 13:18:07 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "azurerm_resource_group" "terraform_resource_group" {
+  name     = "terraform_resource_group"
+  location = "eastus2"
+  tags = {
+    AN-GROUP-NAME   = "tatiterraform"
+    BusinessContact = "tatjana.utvic@abbott.com"
+          }
+            }
